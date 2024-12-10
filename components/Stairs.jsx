4ec03a9f8a3e7387d01db49e4864e2bc870cbc43
Muhammad Azeem <abdulazeem5476@gmail.com>
2024-12-10 13:21:15 +0500

    added

---
+++ components/Stairs.jsx
@@ -0,0 +1,46 @@
+import { motion } from "framer-motion";
+
+// -- Variants
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
+// calculate the reserve index for staggred delay
+function reserveIndex(index) {
+  const totalSteps = 6; // number of steps
+  return totalSteps - index - 1;
+}
+
+function Stairs() {
+  return (
+    <>
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reserveIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
+    </>
+  );
+}
+
+export default Stairs;
